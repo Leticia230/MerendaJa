b8e4b42d6b7d4ddca4b6441f4fb3b8baced572b8
My590 <milenabmiranda1234@gmail.com>
2026-09-15 20:04:02 -0300

    melhorei a segurança

--- components/firebaseConfig.ts
+++ components/firebaseConfig.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDt_bEOP4sRzAdyP995idxYhkQbExRfCEA",
-  authDomain: "merendaja.firebaseapp.com",
-  projectId: "merendaja",
-  storageBucket: "merendaja.firebasestorage.app",
-  messagingSenderId: "184963829406",
-  appId: "1:184963829406:web:87db0942c0dd077e71f58f",
-  measurementId: "G-G4JRSBT1L7"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
